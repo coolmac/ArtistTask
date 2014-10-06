30bb2c5fdd7b8a18b92e80eb584cb4ade944d018
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-10-06 12:42:56 +0530

    route fixes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'home#index'
+   match "searchartist" => "homes#search_artist", :as => "searchartist",via: [:get, :post]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
